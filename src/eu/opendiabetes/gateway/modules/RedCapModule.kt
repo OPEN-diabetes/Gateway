89e264919df2b0ab9832d96edfe96f288d0c19a2
TebbeUbben <tebbe@ubben.eu>
2020-07-11 00:19:08 +0200

    Information sheets for participation links

--- src/eu/opendiabetes/gateway/modules/RedCapModule.kt
+++ src/eu/opendiabetes/gateway/modules/RedCapModule.kt
@@ -1,13 +1,19 @@
 package eu.opendiabetes.gateway.modules
 
+import eu.opendiabetes.gateway.database.ParticipationLink
+import eu.opendiabetes.gateway.templates.respondConsentNotGivenTemplate
+import eu.opendiabetes.gateway.templates.respondConsentTemplate
 import eu.opendiabetes.gateway.utils.RedCapAPI
 import eu.opendiabetes.gateway.utils.*
 import io.ktor.application.Application
+import io.ktor.application.ApplicationCall
 import io.ktor.application.call
 import io.ktor.http.HttpStatusCode
+import io.ktor.request.receiveParameters
 import io.ktor.response.respond
 import io.ktor.response.respondRedirect
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.routing.route
 import io.ktor.routing.routing
 
@@ -18,7 +24,7 @@ fun Application.redCapModule() {
         getStringProperty("gateway.redcap.token")
     )
     routing {
-        get("survey") {
+        get("/survey") {
             val participant = call.participant
             if (participant != null) {
                 if (participant.surveyRecordId == null) {
@@ -32,25 +38,40 @@ fun Application.redCapModule() {
                 call.respondRedirect("login")
             }
         }
-        route("p/{participantId}/{secret}") {
+        route("/p/{id}/{secret}") {
             get("/") {
-                val id = call.parameters["participantId"]?.toLongOrNull()
-                val secret = call.parameters["secret"]
-                if (id != null && secret != null) {
-                    val participationLink = this@redCapModule.database.getParticipationLink(id)
-                    if (participationLink?.secret == secret) {
+                call.checkParticipationLink() {
+                    respondConsentTemplate()
+                }
+            }
+            post("/") {
+                call.checkParticipationLink() { participationLink ->
+                    if (receiveParameters().contains("consent")) {
                         if (participationLink.surveyRecordId == null) {
-                            val recordId = redCapAPI.createRecord(id, participationLink.enrollmentType)
-                            this@redCapModule.database.setSurveyRecordIdForParticipationLink(id, recordId)
+                            val recordId = redCapAPI.createRecord(participationLink.id, participationLink.enrollmentType)
+                            this@redCapModule.database.setSurveyRecordIdForParticipationLink(participationLink.id, recordId)
                             call.respondRedirect(redCapAPI.exportSurveyQueueLink(recordId))
                         } else {
                             call.respondRedirect(redCapAPI.exportSurveyQueueLink(participationLink.surveyRecordId))
                         }
                     } else {
-                        call.respond(HttpStatusCode.Unauthorized)
+                        call.respondConsentNotGivenTemplate("/p/${participationLink.id}/${participationLink.secret}")
                     }
                 }
             }
+        }
+    }
+}
+
+private suspend fun ApplicationCall.checkParticipationLink(block: suspend ApplicationCall.(ParticipationLink) -> Unit) {
+    val id = parameters["id"]?.toLongOrNull()
+    val secret = parameters["secret"]
+    if (id != null && secret != null) {
+        val participationLink = application.database.getParticipationLink(id)
+        if (participationLink?.secret == secret) {
+            block(participationLink)
+        } else {
+            respond(HttpStatusCode.Unauthorized)
         }
     }
 }
