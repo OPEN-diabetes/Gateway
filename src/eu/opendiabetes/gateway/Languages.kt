66f31bd215de9e73688ce6e150e5a05c84b2f1ae
Saskia <chrisingermany@gmx.de>
2020-08-25 13:23:01 +0200

    Update Languages.kt
    Die Überschrift soll minimal abgeändert werden, dafür beim Begrüßungstext ganz wegfallen, um Dopplungen zu vermeiden.

--- src/eu/opendiabetes/gateway/Languages.kt
+++ src/eu/opendiabetes/gateway/Languages.kt
@@ -120,7 +120,7 @@ object English : Language() {
     override val genderNotice: String? = null
     override val hiThere = "Welcome to the OPEN project!"
     override val thanksForHelpingUs =
-        "Thank you for helping us on our research on DIYAPS by participating in this survey!"
+        "Thank you for your interest and willingness to participate in our survey!"
     override val submit = "Submit"
     override val unknownId = "This Participant ID does not exist."
     override val newParticipant = "New Participant"
