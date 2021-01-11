9a7d4dc3450e226056335740721ed6c46961b156
Saskia <missdazzle@web.de>
2021-01-11 16:22:17 +0100

    Update ParentUsers.kt

--- src/eu/opendiabetes/gateway/language/info_sheets/english/ParentUsers.kt
+++ src/eu/opendiabetes/gateway/language/info_sheets/english/ParentUsers.kt
@@ -20,23 +20,19 @@ const val INFO_SHEET_PARENT_USERS = """
 <p>You have been invited to take part because you are the parent/carer of a DIYAPS user.</p>
 <h1>What will happen if I decide to take part in this research study?</h1>
 <p>If you kindly agree to take part, you will be first given a unique <b>participant ID</b> which will guarantee your anonymity in the survey. The ID allows you to take a break and come back where you have left off at a later stage or login on another device and continue from there. Therefore, <b>please keep it safe</b> by noting it down, emailing it to yourself or taking a picture of it as you may need it later.</p></p> 
-<p>You will then be asked to <b>complete a series of questionnaires</b> designed to access the quality of life and well-being of you and your child. Altogether, it should not take you more than 20-30 minutes. These questionnaires will focus on:</p>
+<p>You will then be asked to <b>complete a series of questionnaires</b> designed to access the health of your child. Altogether, it should not take you more than 10-15 minutes. These questionnaires will focus on:</p>
 <ul>
     <li>Demographic information (about yourself and your child)</li>
     <li>Information about your child’s health and diabetes</li>
-    <li>Your and your child’s general experiences of living with diabetes</li>
-    <li>Your general sense of well-being caring for someone with diabetes</li>
-    <li>Your sleep quality and mood</li>
     <li>Your and your child's general experiences of building and maintaining a DIYAPS</li>
 </ul>
 <p>To further assist our research, we would kindly invite you and your child to <b>become members of</b> a data repository site called ‘Open Humans’. <b>Open Humans</b> allows people to <b>anonymously donate</b> their health related data (including <b>device data</b>, e.g. from their glucose monitoring, or Nightscout, etc.) for research purposes. Once you sign up to Open Humans, we invite you to <b>join our project</b> on the Open Humans platform called the ‘Open Project’. This should take you no longer than 2-3 minutes.</p>
-<p>Choosing to sign up to Open Humans and joining the OPEN project on Open Humans is <b>optional</b> and you decide what data you would like to share with us (if at all), and whether you and your child would like to <b>participate in any further follow on studies</b>. Bear in mind that your participation in the OPEN project and future studies will greatly assist us to enhance the existing scientific evidence on DIYAPS.</p>
+<p>Choosing to sign up to Open Humans and joining the OPEN project on Open Humans is <b>optional</b> and you decide what data you would like to share with us (if at all), and whether you and your child would like to <b>participate in any further follow-up studies</b>. Bear in mind that your participation in the OPEN project and future studies will greatly assist us to enhance the existing scientific evidence on DIYAPS.</p>
 <p>If you (and your child) are happy to sign up to the Open Humans platform, instructions on how to connect your existing Open Humans account — or create a new one and connect this — are provided on the <a href="https://open-diabetes.eu/en/open-survey/" target="_blank">OPEN website</a>.</p>  
-<p>Lastly, if your child is of adolescent age (13-17 years old), and they would like to complete a shorter version of the survey about their quality of life, you will also be able to provide them with a <b>participation link</b>. This would help enhance our understanding of the impact of DIYAPS on the quality of life of both you and your child.  Do not share this with anyone but your child, who should use this link to complete the questionnaire, so that your answers can be combined. It should not take them any longer than 10-15 minutes to complete and responses will be <b>confidential</b>. That means you will not be able to see your child’s responses and vice-versa. Sharing the participation link with your child is optional.</p>
 <h1>How will the data be used?</h1> 
-<p>The pseudonymised responses to the survey will be analysed by our team of researchers. <b>If you choose to upload your child’s device data to the Open Humans project, we will combine them with your responses to the survey described above to create a more complete picture of the impact of DIYAPS on the lives of people with diabetes</b>. It will also help us to (anonymously) stay “in touch” and inform you about any follow-up studies in the future.</p>
-<p>Once the OPEN project is completed your anonymised data will be available to researchers upon request. To protect your privacy, any information that might lead to your identity will be removed from your data set before being accessed by any researchers.</p>
-<p>The organisations in the OPEN consortium involved in accessing your data will include:</p>
+<p>The pseudonymized responses to the survey will be analyzed by our team of researchers. <b>If you choose to upload your child’s device data to the Open Humans project, we will combine them with your responses to the survey described above to create a more complete picture of the impact of DIYAPS on the lives of people with diabetes</b>. It will also help us to (anonymously) stay “in touch” and inform you about any follow-up studies in the future.</p>
+<p>Once the OPEN project is completed your anonymized data will be available to researchers upon request. To protect your privacy, any information that might lead to your identity will be removed from your data set before being accessed by any researchers.</p>
+<p>The organizations in the OPEN consortium involved in accessing your data will include:</p>
 <ul>
     <li>University College Dublin, Ireland</li>
     <li>Charité - Universitätsmedizin Berlin, Germany</li>
@@ -47,7 +43,7 @@ const val INFO_SHEET_PARENT_USERS = """
 </ul>
 <h1>How will you protect our privacy?</h1>
 <p>The design of this study is in line with all EU (GDPR), Irish (Data Protection Act, 2018), and UCD (UCD ethics Committee) ethical and data protection regulations. All personal information will be made unidentifiable before being transmitted and stored in the OPEN Humans server. Every precaution will be taken to respect your privacy, in line with the General Data Protection Regulations (GDPR).</p> 
-<p>As stated above, any information that you share that may potentially inadvertently make you identifiable will be removed - this process is called pseudonymisation.</p>  
+<p>As stated above, any information that you share that may potentially inadvertently make you identifiable will be removed - this process is called pseudonymization.</p>  
 <p>Please note that only adults (aged 18+) who are able to provide consent are allowed to participate. Your child (<b>if</b> 13-17 years of age) will be asked to sign in the assent form before completing their version of the survey.</p> 
 <h1>What are the benefits of taking part in this research study?</h1>
 <p>There are no direct, material benefits expected from taking part in this study. However, you will be helping to improve the overall scientific knowledge about DIYAPS and its use. As a consequence, your participation and experience may help the diabetes community.</p> 
@@ -63,7 +59,7 @@ const val INFO_SHEET_PARENT_USERS = """
 <h1>How will I find out what happens with this project?</h1>
 <p>The OPEN team would like you to engage with this project as much as possible. That is why we have developed a website for the OPEN project. On <a href="https://open-diabetes.eu">this website</a>, we provide regular updates on the findings of our project and you may also sign up to our newsletter to receive regular email updates. All the results of our research will be published in a way which will be accessible and free of charge to the public.</p>
 <h1>Contact Details</h1>
-<p>If any participant or participant’s caregiver would like to contact a member of the research team <i>(you can see us <a href="https://open-diabetes.eu/about_us/consortium">here</a> on our website)</i> for any research-related reason, they can do so via the contact information below. If any query is not responded to in a satisfactory manner, then contact can be made with a Data Protection Officer at UCD, whose details are also provided below. For general data protection enquiries, please contact the UCD Data Protection office (general enquiries). Finally, if none of the UCD contacts have given a satisfactory response, details on the Data Protection Commission are provided below.</p>
+<p>If any participant or participant’s caregiver would like to contact a member of the research team <i>(you can see us <a href="https://open-diabetes.eu/about_us/consortium">here</a> on our website)</i> for any research-related reason, they can do so via the contact information below. If any query is not responded to in a satisfactory manner, then contact can be made with a Data Protection Officer at UCD, whose details are also provided below. For general data protection inquiries, please contact the UCD Data Protection office (general enquiries). Finally, if none of the UCD contacts have given a satisfactory response, details on the Data Protection Commission are provided below.</p>
 <div id="contact-details">
     <div class="contact-detail">
         <span><b>Dr Shane O’Donnell</b></span>
