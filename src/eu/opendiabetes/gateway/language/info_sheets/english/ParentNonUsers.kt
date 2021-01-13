d675bce24bb2327ef6980b78283ff6fe020e7cd0
Saskia <missdazzle@web.de>
2021-01-13 18:42:22 +0100

    Update ParentNonUsers.kt

--- src/eu/opendiabetes/gateway/language/info_sheets/english/ParentNonUsers.kt
+++ src/eu/opendiabetes/gateway/language/info_sheets/english/ParentNonUsers.kt
@@ -2,41 +2,31 @@ package eu.opendiabetes.gateway.language.info_sheets.english
 
 const val INFO_SHEET_PARENT_NON_USERS = """
 <h1>Introduction</h1>
-<p>The OPEN team invites you and your child to take part in a global study that examines the impact of Do-It-Yourself Artificial Pancreas Systems (DIYAPS) on the health and well-being of users. The following information explains what to expect if you choose on your child's behalf to take part in the research. <b>Please read it carefully. If you do not understand some of the information and/or would like to discuss anything related to the project, feel free to contact a member of our research team.</b></p>
+<p>The OPEN team invites you to take part in a global study that examines in what way the innovations being developed by the Do-It-Yourself Artificial Pancreas Systems (DIYAPS) community might benefit all people living with diabetes. The following information explains what to expect if you choose to take part in the research. <b>Please read it carefully. If you do not understand some of the information and/or would like to discuss anything related to the project, feel free to contact a member of our research team.</b></p>
 <h1>Name of Study</h1> 
 <p>OPEN (Outcomes of Patients’ Evidence with Novel, Do-it-Yourself Artificial Pancreas Technology)</p>
 <h1>Name of Principal Investigator</h1>
 <p>Dr Shane O’Donnell</p>
 <h1>What is this research about?</h1>
-<p>A growing number of people living with diabetes are choosing to build DIYAPS to help with the management of their condition. However, more research is needed to understand if and how people living with diabetes benefit from using these systems. This study has four main aims:</p> 
-<ol>
-    <li>to examine the clinical and</li>
-    <li>quality of life outcomes of DIYAPS users and non-users</li>
-    <li>to identify potential technical improvements that could be made to artificial pancreas technology of all kinds, and</li>
-    <li>to explore the difficulties and challenges that people face in building DIYAPS, as well as why some choose not to use DIYAPS</li>
-</ol>
+<p>A growing number of people living with diabetes are choosing to build DIYAPS to help with the management of their condition. However, more research is needed to understand if and how people living with diabetes benefit from using these systems. The primary aim of this study is to document the difficulties and challenges that people face in building DIYAPS, as well as why some choose not to use DIYAPS.</p> 
 <h1>Why is OPEN doing this research?</h1>
-<p>By carrying out this research study, OPEN will be able to build evidence about the effectiveness of DIYAPS and potential further improvements in the technology that could be introduced in the future.</p>
+<p>By carrying out this research study, OPEN aims to understand the barriers to uptake of DIYAPS solutions.</p>
 <h1>Why am I being invited to take part?</h1>
-<p>You have been invited to take part because you are the parent/carer of a child with diabetes and a member of the DIYAPS online community.</p>
+<p>You have been invited to take part because you are the parent or caretaker of a child that has diabetes and you are a member of the DIYAPS community.</p>
 <h1>What will happen if we decide to take part in this research study?</h1>
-<p>If you kindly agree to take part, you will be first given a unique <b>participant ID</b> which will guarantee your anonymity in the survey. The ID allows you to take a break and come back where you have left off at a later stage or login on another device and continue from there. Therefore, <b>please keep it safe</b> by noting it down, emailing it to yourself or taking a picture of it as you may need it later.</p></p> 
-<p>You will then be asked to <b>complete a series of questionnaires</b> designed to access the quality of life and well-being of you and your child. Altogether, it should not take you more than 20-30 minutes. These questionnaires will focus on:</p>
+<p>If you agree to take part, you will be first given a unique <b>participant ID</b> which will guarantee your anonymity in the survey. The ID allows you to take a break and come back where you have left off at a later stage or login on another device and continue from there. Therefore, <b>please keep it safe by noting it down, emailing it to yourself or taking a picture of it as you may need it later</b>.</p></p> 
+<p>You will then be asked to <b>complete some questionnaires</b>. Altogether, it should not take you more than 10-15 minutes. These questionnaires will focus on:</p>
 <ul>
-    <li>Demographic information (about yourself and your child)</li>
-    <li>Information about your child’s health and diabetes</li>
-    <li>Your and your child’s general experiences of living with diabetes</li>
-    <li>Your general sense of well-being caring for someone with diabetes</li>
-    <li>Your sleep quality and mood</li>
+    <li>Your and your child`s demographic information</li>
     <li>Your experiences of being a member of an online DIYAPS community</li>
     <li>The challenges you may have experienced or anticipate in building a DIYAPS</li>
 </ul>
-<p>To further assist our research, we would kindly invite you to <b>become a member of</b> a data repository site called ‘<b>Open Humans</b>’. Open Humans allows people to anonymously donate their health related data (including device data i.e. from their glucose monitoring, or Nightscout, etc.) for research purposes. Once you sign up to Open Humans, we invite you to <b>join our project</b> on the Open Humans platform called the ‘Open Project’. This will allow us to <b>anonymously contact you to request your participation in future OPEN studies</b>. The process should take you no longer than 2-3 minutes.</p>
+<p>At the end of the survey, we will ask you if you would like to further assist our research by completing an additional questionnaire regarding your child’s most recent diabetes related medical history. We will also ask if you would like to sign up for follow-up studies run by OPEN in the future. For this, you can <b>become a member of the OPEN project</b> on a data repository site called ‘<b>Open Humans</b>’. This will allow us to <b>anonymously communicate with you</b> to ask about your participation in future studies as data protection laws do not allow us to obtain or use personal email accounts for communication. <b>Instructions</b> on how to connect your existing Open Humans account — or create a new one and connect this — <b>are provided on the</b> <a href="https://open-diabetes.eu/en/open-survey/" target="_blank">OPEN website</a>. Please note that these additional steps are completely optional.</p>
 <p>Choosing to sign up to Open Humans and joining the OPEN project on Open Humans is <b>optional</b> and you decide if you would like to participate in any further follow on studies. Bear in mind that your participation in the OPEN project and future studies will greatly assist us to enhance the existing scientific evidence on DIYAPS.</p>
 <p>If you are happy to sign up to the Open Humans platform, instructions on how to connect your existing Open Humans account — <b>or</b> create a new one and connect this — are provided on the <a href="https://open-diabetes.eu/en/open-survey/" target="_blank">OPEN website</a>.</p>  
 <h1>How will the data be used?</h1> 
-<p>The pseudonymised responses to the survey will be analysed by our team of researchers. Once the OPEN project is completed your and your child’s anonymised data will be available to researchers upon request. To protect your and your child’s privacy, any information that might lead to your identity will be removed from your data set before being accessed by any researchers.</p>
-<p>The organisations in the OPEN consortium involved in accessing your data will include:</p>
+<p>The pseudonymized responses to the survey will be analyzed by our team of researchers. Once the OPEN project is completed your anonymized data will be available to researchers upon request. To protect your privacy, any information that might lead to your identity will be removed from your data set before being accessed by any researchers.</p>
+<p>The organizations in the OPEN consortium involved in accessing your data will include:</p>
 <ul>
     <li>University College Dublin, Ireland</li>
     <li>Charité - Universitätsmedizin Berlin, Germany</li>
@@ -47,17 +37,17 @@ const val INFO_SHEET_PARENT_NON_USERS = """
 </ul>
 <h1>How will you protect my privacy?</h1>
 <p>The design of this study is in line with all EU (GDPR), Irish (Data Protection Act, 2018) and UCD (UCD ethics Committee) ethical and data protection regulations. Every precaution will be taken to respect your privacy, in line with the General Data Protection Regulations (GDPR).</p> 
-<p>As stated above, any information that you share that may potentially inadvertently make you identifiable will be removed - this process is called pseudonymisation.</p>  
+<p>As stated above, any information that you share that may potentially inadvertently make you identifiable will be removed - this process is called pseudonymization.</p>  
 <p>Please note that only adults (aged 18+) who are able to provide consent are allowed to participate.</p> 
 <h1>What are the benefits of taking part in this research study?</h1>
 <p>There are no direct, material benefits expected from taking part in this study. However, you will be helping to improve the overall scientific knowledge about DIYAPS systems and its use. As a consequence, your participation and experience may help the diabetes community.</p> 
 <h1>What are the risks of taking part in this research study?</h1>
-<p>There are no anticipated risks expected from taking part in this study. The only discomfort you may have is related to the time you have to dedicate to complete the survey. The questionnaires that have been selected for use have been tried and tested among a large population of parents/carers of a child with diabetes and have been designed to minimize the risk of causing upset and distress. However, if you do experience this or any other aspect of the study as distressing, you are free to stop participating at any time, without personal consequence, we recommend contacting your GP/primary care physician or local diabetes organizations who may be able to provide you with psychological and practical support. In addition, you may also find the following webpages helpful:</p>
+<p>There are no anticipated risks expected from taking part in this study. However, if you do experience any aspect of the study as distressing, you are free to stop participating at any time, without personal consequence, we recommend contacting your GP/primary care physician or local diabetes organizations who may be able to provide you with psychological and practical support. However, if they are not in a position to provide support, you may find information on the following webpages helpful:</p>
 <ul>
     <li><a href="https://acbrd.org.au/diabetes-and-emotional-health-factsheets/" target="_blank">Diabetes and emotional health factsheets</a></li>
     <li><a href="https://www.jdrf.org/t1d-resources/living-with-t1d/mental-health/" target="_blank">Mental Health</a></li>
 </ul>
-<p>If you choose to provide detailed personal information in a free text field, there is a small chance that you may identify yourself within the questionnaire to OPEN researchers. However, this information will be removed before the data set is shared with the rest of the OPEN research team.</p>
+<p>If you choose to provide detailed personal information in a free text field, there is a small chance that you may identify yourself or your child within the questionnaire to OPEN researchers. However, this information will be removed before the data set is shared with the rest of the OPEN research team.</p>
 <h1>Can I change my mind at any stage and withdraw from the study?</h1>
 <p>Yes! If you decide to participate and subsequently change your mind, you may withdraw from the study at any time. You may also choose to remove your data from future analyses of the OPEN project. To do so, please contact our research team through the details provided below. You can also have the option to leave the OPEN project on Open Humans at any time and remove any data that you have uploaded to the platform. Please note that in both instances we can only delete your data if you provide your Participant ID from the survey.</p>
 <h1>How will I find out what happens with this project?</h1>
