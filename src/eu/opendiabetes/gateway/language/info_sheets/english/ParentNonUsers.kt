a4ee0725324d1ed2958c638902082e34e6092b41
Saskia <missdazzle@web.de>
2021-01-14 11:26:07 +0100

    Update ParentNonUsers.kt

--- src/eu/opendiabetes/gateway/language/info_sheets/english/ParentNonUsers.kt
+++ src/eu/opendiabetes/gateway/language/info_sheets/english/ParentNonUsers.kt
@@ -17,7 +17,7 @@ const val INFO_SHEET_PARENT_NON_USERS = """
 <p>If you agree to take part, you will be first given a unique <b>participant ID</b> which will guarantee your anonymity in the survey. The ID allows you to take a break and come back where you have left off at a later stage or login on another device and continue from there. Therefore, <b>please keep it safe by noting it down, emailing it to yourself or taking a picture of it as you may need it later</b>.</p></p> 
 <p>You will then be asked to <b>complete some questionnaires</b>. Altogether, it should not take you more than 10-15 minutes. These questionnaires will focus on:</p>
 <ul>
-    <li>Your and your child`s demographic information</li>
+    <li>Your demographic information</li>
     <li>Your experiences of being a member of an online DIYAPS community</li>
     <li>The challenges you may have experienced or anticipate in building a DIYAPS</li>
 </ul>
