0dcea18986daeb64345cdd5c756eccaec84d6c12
TebbeUbben <tebbe@ubben.eu>
2021-01-29 15:51:20 +0100

    Move HCP entry up

--- src/eu/opendiabetes/gateway/templates/TodosTemplate.kt
+++ src/eu/opendiabetes/gateway/templates/TodosTemplate.kt
@@ -1,10 +1,9 @@
 package eu.opendiabetes.gateway.templates
 
-import eu.opendiabetes.gateway.language.Language
 import eu.opendiabetes.gateway.utils.OpenHumansAPI
 import eu.opendiabetes.gateway.utils.language
-import io.ktor.application.ApplicationCall
-import io.ktor.http.formUrlEncode
+import io.ktor.application.*
+import io.ktor.http.*
 import kotlinx.html.*
 
 suspend fun ApplicationCall.respondTODOsTemplate(
@@ -34,7 +33,7 @@ suspend fun ApplicationCall.respondTODOsTemplate(
                                 raw(language.thanksForFillingOutSurvey)
                             }
                         }
-                    } else{
+                    } else {
                         text(language.fillOutSurvey)
                         p {
                             unsafe {
@@ -49,34 +48,6 @@ suspend fun ApplicationCall.respondTODOsTemplate(
                         text(language.thanksToParticipants)
                     }
                 }
-            }
-            li {
-                text(language.linkToOpenHumans)
-                when {
-                    dataSources == null -> p {
-                        unsafe {
-                            raw(language.linkToOpenHumansTextSetup)
-                        }
-                    }
-                    dataSources.isEmpty() -> p("warning") {
-                        unsafe {
-                            raw(language.linkToOpenHumansTextDataSources)
-                        }
-                    }
-                    else -> {
-                        p {
-                            unsafe {
-                                raw(language.linkToOpenHumansTextDataSources)
-                            }
-                        }
-                        ul {
-                            if (dataSources.contains(OpenHumansAPI.NIGHTSCOUT_ID)) li { span("success") { text(language.nightscoutDataTransfer) } }
-                            if (dataSources.contains(OpenHumansAPI.ANDROIDAPS_ID)) li { span("success") { text(language.androidAPSUploader) } }
-                            if (dataSources.contains(OpenHumansAPI.SELFIES_ID)) li { span("success") { text(language.dataSelfie) } }
-                        }
-                    }
-                }
-                a("/openhumans", target = "_blank", classes =  "button") { text(if (dataSources == null) language.setup else language.setupAgain) }
             }
             if (showHCPSurvey) {
                 li {
@@ -108,35 +79,41 @@ suspend fun ApplicationCall.respondTODOsTemplate(
                     }
                 }
             }
+            li {
+                text(language.linkToOpenHumans)
+                when {
+                    dataSources == null -> p {
+                        unsafe {
+                            raw(language.linkToOpenHumansTextSetup)
+                        }
+                    }
+                    dataSources.isEmpty() -> p("warning") {
+                        unsafe {
+                            raw(language.linkToOpenHumansTextDataSources)
+                        }
+                    }
+                    else -> {
+                        p {
+                            unsafe {
+                                raw(language.linkToOpenHumansTextDataSources)
+                            }
+                        }
+                        ul {
+                            if (dataSources.contains(OpenHumansAPI.NIGHTSCOUT_ID)) li { span("success") { text(language.nightscoutDataTransfer) } }
+                            if (dataSources.contains(OpenHumansAPI.ANDROIDAPS_ID)) li { span("success") { text(language.androidAPSUploader) } }
+                            if (dataSources.contains(OpenHumansAPI.SELFIES_ID)) li { span("success") { text(language.dataSelfie) } }
+                        }
+                    }
+                }
+                a(
+                    "/openhumans",
+                    target = "_blank",
+                    classes = "button"
+                ) { text(if (dataSources == null) language.setup else language.setupAgain) }
+            }
         }
         a("/logout") {
             id = "go-back"
             text(language.signOut)
         }
     }
-
-private fun LI.participationLink(language: Language, title: String, desc: String, link: String) {
-    text(title)
-    p {
-        unsafe {
-            raw(desc)
-        }
-    }
-    input(type = InputType.url, classes = "participation-link") {
-        readonly = true
-        value = link
-    }
-    val mailtoLink = "mailto:?" + listOf(
-        "subject" to language.invitationToSurvey,
-        "body" to language.invitationText(link)
-    ).formUrlEncode().replace("+", "%20")
-    a(mailtoLink, "_blank", "email-link") {
-        text("Share via e-mail")
-    }
-}
-
-sealed class ParticipationLinkTODO(val link: String) {
-    class Parent(link: String) : ParticipationLinkTODO(link)
-    class Child(link: String) : ParticipationLinkTODO(link)
-    class Partner(link: String) : ParticipationLinkTODO(link)
-}
