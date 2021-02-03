6f969820967a47d41f2f3788412c644372fdfb3a
Saskia <missdazzle@web.de>
2021-02-03 21:34:46 +0100

    Update HCPStudyHCPs.kt

--- src/eu/opendiabetes/gateway/language/info_sheets/english/HCPStudyHCPs.kt
+++ src/eu/opendiabetes/gateway/language/info_sheets/english/HCPStudyHCPs.kt
@@ -16,7 +16,7 @@ const val INFO_SHEET_HCP_STUDY_HCPS = """
 </ol>
 <p><b>This particular study adds a fourth aim:</b></p>
 <ol>
-    <li>to assess the level of agreement between the people with diabetes' self-reported medical outcomes and their outcomes from medical charts reported by their healthcare professional.</li>
+    <li>to assess the level of agreement between the people with diabetes' self-reported medical outcomes and their outcomes from medical charts reported by their healthcare professional</li>
 </ol>
 <h1>Why is OPEN doing this research?</h1>
 <p>By carrying out this research study, OPEN will be able to build reliable evidence about the effectiveness of DIYAPS and potential further improvements in the technology that could be introduced in the future. This part — focusing on the validity of self-reported outcomes — requires your contribution as HCPs.</p>
